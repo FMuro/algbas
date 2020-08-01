4647ec2100a1e3e1774d78d733f58302c744727d
FMuro <fmuro@us.es>
2020-08-01 04:08:58 +0200

    the SVG resizing is in this commit

--- pics/script.sh
+++ pics/script.sh
@@ -2,7 +2,8 @@
 
 pdflatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error pics.tex
 cd tikz
-for f in *.pdf; do 
-  pdftocairo -svg "$f" "../../static/images/${f%.*}.svg"
+for f in *.pdf; do
+  pdftocairo -svg "$f" "${f%.*}.svg"
+  rsvg-convert -f svg -h 300 "${f%.*}.svg" > ""../../static/images/${f%.*}.svg""
 done
 cd ..
