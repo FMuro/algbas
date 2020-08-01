54b214d907f8728e70aea4b64a62e56977eb242b
FMuro <fmuro@us.es>
2020-08-01 03:37:44 +0200

    auto generate PNG from PDF using TiKZ and LaTeX

--- pics/script.sh
+++ pics/script.sh
@@ -3,6 +3,18 @@
 pdflatex -shell-escape -synctex=1 -interaction=nonstopmode -file-line-error pics.tex
 cd tikz
 for f in *.pdf; do 
-  pdftocairo -svg "$f" "../../static/images/${f%.*}.svg"
+# EACH OF THE FOLLOWING COMMMENTS SHOULD BE BESIDE OF THE LINES BELOW BUT BASH SCRIPS DON'T ALLOW TO COMMENT SPLIT COMMANDS IN THIS WAY
+# PDF to PNG (and other formats) conversion tool
+# convert to PNG
+# just converts the first page of the PDF (which in this case has a single page anyway) and doesn't add counter to the output file name
+# output height in pixels
+# keep aspect ratio
+# take filename.pdf to filename.png
+  pdftocairo \
+  -png \
+  -singlefile \
+  -scale-to-y 300 \
+  -scale-to-x -1 \
+  "$f" "../../static/images/${f%.*}"
 done
 cd ..
